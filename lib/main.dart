1e35aa6b1fdf00a48d1ed62058eeb243cf72c936
Stypox <stypox@pm.me>
2022-03-27 09:21:36 +0200

    Load data from remote server

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Insignio',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Insignio'),
@@ -92,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _loadPill() async {
     var response = await http.get(
-      Uri.parse('http://10.0.2.2:8000/pills/random'),
+      Uri.parse('http://insignio.mindshub.it/pills/random'),
       headers: {'Accept': 'application/json'},
     );
 
