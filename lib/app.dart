c69bf6422ffc4876fe93e5f45a726dedc5eefe82
Stypox <stypox@pm.me>
2023-02-27 17:45:44 +0100

    Add dynamic theming

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:insignio_frontend/map/map_page.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 
 class InsignioApp extends StatelessWidget {
   const InsignioApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Insignio",
-      theme: ThemeData(
-        useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      home: MapPage(),
-    );
+    return DynamicColorBuilder(builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+      return MaterialApp(
+        title: "Insignio",
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightDynamic ?? const ColorScheme.light()
+        ),
+        darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkDynamic ?? const ColorScheme.dark()
+        ),
+        home: MapPage(),
+      );
+    });
   }
 }
