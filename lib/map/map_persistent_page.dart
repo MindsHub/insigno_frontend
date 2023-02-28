3fb76378f1ebd8e2cf99f22ac6c1b035841791f7
Stypox <stypox@pm.me>
2023-02-28 15:22:00 +0100

    Show add button only if logged in

--- lib/map/map_persistent_page.dart
+++ lib/map/map_persistent_page.dart
@@ -3,6 +3,7 @@ import 'package:get_it_mixin/get_it_mixin.dart';
 import 'package:insignio_frontend/map/location.dart';
 import 'package:insignio_frontend/map/map_widget.dart';
 
+import '../auth/authentication.dart';
 import '../di/setup.dart';
 
 class MapPersistentPage extends StatefulWidget with GetItStatefulWidgetMixin {
@@ -19,22 +20,22 @@ class _MapPersistentPageState extends State<MapPersistentPage>
     super.build(context);
 
     final position = watchStream((LocationProvider location) => location.getLocationStream(),
-        getIt<LocationProvider>().lastLocationInfo())
+            getIt<LocationProvider>().lastLocationInfo())
         .data;
-    print((position?.position.toString() ?? "null") +
-        " " +
-        (position?.servicesEnabled.toString() ?? "boh") +
-        " " +
-        (position?.permissionGranted.toString() ?? "boh"));
+    final bool isLoggedIn = watchStream(
+                (Authentication authentication) => authentication.getIsLoggedInStream(),
+                getIt<Authentication>().isLoggedIn())
+            .data ??
+        false;
 
     return Scaffold(
       body: MapWidget(),
-      floatingActionButton: position?.position == null
+      floatingActionButton: (position?.position == null || !isLoggedIn)
           ? null
           : FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
       // TODO share floatingActionButtonAnimator across pages
     );
   }
