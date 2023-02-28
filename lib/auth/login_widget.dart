24db7e9e6f6821fca4bdb87d46da1522ca74364a
Stypox <stypox@pm.me>
2023-02-28 17:09:37 +0100

    Remove unnecessary Center

--- lib/auth/login_widget.dart
+++ lib/auth/login_widget.dart
@@ -38,51 +38,50 @@ class _LoginWidgetState extends State<LoginWidget> with GetItStateMixin<LoginWid
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Padding(
-            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-            child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "Login to Insignio!",
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormField(
-                    initialValue: "john@gmail.com",
-                    decoration: const InputDecoration(labelText: "Email"),
-                    onSaved: (value) => username = value,
-                  ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    initialValue: "NiceDoggo1",
-                    decoration: const InputDecoration(labelText: "Password"),
-                    onSaved: (value) => password = value,
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 16),
-                  if (loginFailed)
-                    const Text(
-                      "Wrong user or password",
-                      style: TextStyle(color: Colors.red),
-                    ),
-                  const SizedBox(height: 16),
-                  loading
-                      ? const CircularProgressIndicator()
-                      : FloatingActionButton(
-                          onPressed: () {
-                            formKey.currentState?.save();
-                            performLogin();
-                          },
-                          child: const Icon(Icons.login),
-                        )
-                ],
+    return Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Login to Insignio!",
+                style: Theme.of(context).textTheme.headlineSmall,
               ),
-            )));
+              const SizedBox(height: 16),
+              TextFormField(
+                initialValue: "john@gmail.com",
+                decoration: const InputDecoration(labelText: "Email"),
+                onSaved: (value) => username = value,
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                initialValue: "NiceDoggo1",
+                decoration: const InputDecoration(labelText: "Password"),
+                onSaved: (value) => password = value,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+              ),
+              const SizedBox(height: 16),
+              if (loginFailed)
+                const Text(
+                  "Wrong user or password",
+                  style: TextStyle(color: Colors.red),
+                ),
+              const SizedBox(height: 16),
+              loading
+                  ? const CircularProgressIndicator()
+                  : FloatingActionButton(
+                      onPressed: () {
+                        formKey.currentState?.save();
+                        performLogin();
+                      },
+                      child: const Icon(Icons.login),
+                    )
+            ],
+          ),
+        ));
   }
 }
