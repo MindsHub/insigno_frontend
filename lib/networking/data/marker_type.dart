d1ba4db8f117ad6a751b3a04ff0f2613d0ab3e0f
Stypox <stypox@pm.me>
2023-03-02 08:51:21 +0100

    Set marker types

--- lib/networking/data/marker_type.dart
+++ lib/networking/data/marker_type.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 enum MarkerType {
-  unknown(-1, Colors.grey, Icons.help),
-  plastic(0, Colors.indigo, Icons.polymer),
-  paper(0, Colors.yellow, Icons.auto_stories),
-  undifferentiated(0, Colors.red, Icons.broken_image),
-  glass(0, Colors.green, Icons.liquor),
-  compost(0, Colors.brown, Icons.compost),
-  electronics(0, Colors.white, Icons.local_laundry_service);
+  unknown(1, Colors.grey, Icons.help),
+  plastic(2, Colors.indigo, Icons.polymer),
+  paper(3, Colors.yellow, Icons.auto_stories),
+  undifferentiated(4, Colors.red, Icons.broken_image),
+  glass(5, Colors.green, Icons.liquor),
+  compost(6, Colors.brown, Icons.compost),
+  electronics(7, Colors.white, Icons.local_laundry_service);
 
   final int id;
   final Color color;
