f1c7e1c60020e291d8ebfbe9b82c76e8a5772358
Stypox <stypox@pm.me>
2023-03-15 12:32:10 +0100

    Improve report as inappropriate dialog with OverflowBar

--- lib/marker/report_as_inappropriate_dialog.dart
+++ lib/marker/report_as_inappropriate_dialog.dart
@@ -8,19 +8,36 @@ class ReportAsInappropriateDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
 
-    return AlertDialog(
-      title: Text(l10n.reportAsInappropriate),
-      content: Text(l10n.confirmReportAsInappropriate),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.pop(context, false),
-          child: Text(l10n.cancel),
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                l10n.confirmReportAsInappropriate,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            OverflowBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              overflowSpacing: 4,
+              children: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context, false),
+                  child: Text(l10n.cancel),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context, true),
+                  child: Text(l10n.ok),
+                ),
+              ],
+            )
+          ],
         ),
-        TextButton(
-          onPressed: () => Navigator.pop(context, true),
-          child: Text(l10n.ok),
-        ),
-      ],
+      ),
     );
   }
 }
