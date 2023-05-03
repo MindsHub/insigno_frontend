a5dd2f5d403714c18504c84ed3d86f1b326def8d
Stypox <stypox@pm.me>
2023-05-03 15:30:28 +0200

    Revert changing main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:insigno_frontend/app.dart';
 import 'package:insigno_frontend/di/setup.dart';
 
 void main() async {
-  runZonedGuarded(() {
-    WidgetsFlutterBinding.ensureInitialized();
-    await configureDependencies();
-    runApp(const InsignoApp());
-  }, (error, stack) {});
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
+  runApp(const InsignoApp());
 }
