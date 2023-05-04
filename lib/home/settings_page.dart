548f227dc1b8942f350110eb369507fd479e694f
Stypox <stypox@pm.me>
2023-05-04 15:29:02 +0200

    Add two information panels to settings page

--- lib/home/settings_page.dart
+++ lib/home/settings_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get_it_mixin/get_it_mixin.dart';
 
 class SettingsPage extends StatefulWidget with GetItStatefulWidgetMixin {
@@ -12,14 +14,69 @@ class SettingsPage extends StatefulWidget with GetItStatefulWidgetMixin {
 class _SettingsPageState extends State<SettingsPage> with GetItStateMixin<SettingsPage> {
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+    final theme = Theme.of(context);
+
     return SingleChildScrollView(
       child: Padding(
-        padding: EdgeInsets.fromLTRB(16, 16 + MediaQuery.of(context).padding.top, 16, 16),
+        padding: EdgeInsets.fromLTRB(8, 8 + MediaQuery.of(context).padding.top, 8, 8),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Card(
+              child: InkWell(
+                onTap: () {},
+                borderRadius: const BorderRadius.all(Radius.circular(16)),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          l10n.insignoDescription,
+                          textAlign: TextAlign.center,
+                          style: theme.textTheme.bodyMedium,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      SvgPicture.asset(
+                        "assets/icons/insigno_logo.svg",
+                        width: MediaQuery.of(context).size.width / 5,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Card(
+              child: InkWell(
+                onTap: () {},
+                borderRadius: const BorderRadius.all(Radius.circular(16)),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          l10n.mindshubDescription,
+                          textAlign: TextAlign.center,
+                          style: theme.textTheme.bodyMedium,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      SvgPicture.asset(
+                        "assets/icons/mindshub_logo.svg",
+                        width: MediaQuery.of(context).size.width / 5,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            if (kDebugMode) const SizedBox(height: 8),
             if (kDebugMode)
               Row(
                 mainAxisSize: MainAxisSize.max,
