b5e24e624c281a2ad76024bebd55cc6ba8e7ca7d
Stypox <stypox@pm.me>
2023-05-05 11:52:46 +0200

    Also allow specifying port in ServerHostHandler

--- lib/networking/server_host_handler.dart
+++ lib/networking/server_host_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:injectable/injectable.dart';
 import 'package:insigno_frontend/pref/preferences_keys.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,36 +12,59 @@ class ServerHostHandler {
   final SharedPreferences _preferences;
   String _scheme;
   String _host;
+  int? _port;
+  final StreamController<String> _uriStringStreamController = StreamController.broadcast();
 
   ServerHostHandler(this._preferences)
       : _scheme = _preferences.getString(serverScheme) ?? _defaultScheme,
-        _host = _preferences.getString(serverHost) ?? _defaultHost;
+        _host = _preferences.getString(serverHost) ?? _defaultHost,
+        _port = _preferences.getInt(serverPort) {
+    _uriStringStreamController.add(getUri("").toString());
+  }
 
   Uri getUri(String path, {Map<String, dynamic>? params}) {
     return Uri(
       scheme: _scheme,
       host: _host,
+      port: _port,
       path: path,
       queryParameters: params,
     );
   }
 
-  void setSchemeHost(String uriString) async {
-    final uri = Uri.parse(uriString);
-    _scheme = uri.scheme;
-    _host = uri.host;
+  Future<void> setSchemeHost(String scheme, String host, int? port) async {
+    if (scheme == _defaultScheme && host == _defaultHost) {
+      return resetSchemeHost();
+    }
+
+    _scheme = scheme;
+    _host = host;
+    _port = port;
+    _uriStringStreamController.add(getUri("").toString());
     await Future.wait([
-      _preferences.setString(serverScheme, uri.scheme),
-      _preferences.setString(serverHost, uri.host),
+      _preferences.setString(serverScheme, scheme),
+      _preferences.setString(serverHost, host),
+      if (port == null) _preferences.remove(serverPort) else _preferences.setInt(serverPort, port),
     ]);
   }
 
-  void resetSchemeHost() async {
+  Future<void> resetSchemeHost() async {
     _scheme = _defaultScheme;
     _host = _defaultHost;
+    _port = null;
+    _uriStringStreamController.add(getUri("").toString());
     await Future.wait([
       _preferences.remove(serverScheme),
       _preferences.remove(serverHost),
+      _preferences.remove(serverPort),
     ]);
+  }
+
+  Stream<String> getUriStringStream() {
+    return _uriStringStreamController.stream;
+  }
+
+  static String getDefaultUriString() {
+    return Uri(scheme: _defaultScheme, host: _defaultHost).toString();
   }
 }
