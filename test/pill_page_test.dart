df5bb045be8a1c296671c4aee886ec157e4edf6d
Stypox <stypox@pm.me>
2023-09-13 21:01:16 +0200

    Fix unused imports

--- test/pill_page_test.dart
+++ test/pill_page_test.dart
@@ -5,9 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:insigno_frontend/home/pill_page.dart';
 import 'package:insigno_frontend/networking/data/pill.dart';
 
