1ebe8ba3655416e9bf3275c77f99dbc85ac0e9b3
Stypox <stypox@pm.me>
2023-10-15 15:04:05 +0200

    Replace introduction_screen library with custom

--- lib/page/introduction_page.dart
+++ lib/page/introduction_page.dart
@@ -1,9 +1,9 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:gif/gif.dart';
 import 'package:insigno_frontend/di/setup.dart';
 import 'package:insigno_frontend/networking/backend.dart';
-import 'package:introduction_screen/introduction_screen.dart';
 
 class IntroductionPage extends StatefulWidget {
   static const routeName = '/introductionPage';
@@ -18,6 +18,7 @@ class IntroductionPage extends StatefulWidget {
 
 class _IntroductionPageState extends State<IntroductionPage> {
   List<String>? imageUrls;
+  int i = 0;
 
   @override
   void initState() {
@@ -39,62 +40,77 @@ class _IntroductionPageState extends State<IntroductionPage> {
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
+    final urls = imageUrls;
 
     return Scaffold(
       backgroundColor: const Color(0xFFC9F687),
-      body: IntroductionScreen(
-        controlsPadding: EdgeInsets.zero,
-        rawPages: [
-          if (imageUrls == null)
-            const Center(
-              child: CircularProgressIndicator(),
-            )
-          else
-            for (var imageUrl in imageUrls!)
-              Center(
-                child: Gif(
-                  image: NetworkImage(imageUrl),
-                  placeholder: (_) => const CircularProgressIndicator(),
-                  autostart: Autostart.once,
-                ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: urls == null
+                    ? const CircularProgressIndicator()
+                    : Gif(
+                        image: NetworkImage(urls[i]),
+                        placeholder: (_) => const CircularProgressIndicator(),
+                        autostart: Autostart.once,
+                      ),
               ),
-        ],
-        onSkip: () => widget.onDone(context),
-        onDone: () => widget.onDone(context),
-        //ClampingScrollPhysics prevent the scroll offset from exceeding the bounds of the content.
-        scrollPhysics: const ClampingScrollPhysics(),
-        showDoneButton: true,
-        showNextButton: true,
-        showSkipButton: true,
-        skip: Text(l10n.introSkip),
-        next: const Icon(Icons.forward),
-        done: Text(l10n.introStart),
-        dotsDecorator: const DotsDecorator(
-          spacing: EdgeInsets.symmetric(horizontal: 2),
-          activeSize: Size(12, 5),
-          activeShape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25.0)),
-          ),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                  child: ElevatedButton(
+                    onPressed: () => widget.onDone(context),
+                    child: Text(l10n.introSkip),
+                  ),
+                ),
+                if (urls != null)
+                  DotsIndicator(
+                    dotsCount: urls.length,
+                    position: i,
+                    decorator: const DotsDecorator(
+                      spacing: EdgeInsets.symmetric(horizontal: 2),
+                      activeSize: Size(12, 5),
+                      activeShape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                      ),
+                    ),
+                    onTap: (position) => setState(() {
+                      i = position;
+                    }),
+                  ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if (urls == null) {
+                          return;
+                        } else if (i < urls.length - 1) {
+                          setState(() {
+                            i = i + 1;
+                          });
+                        } else {
+                          widget.onDone(context);
+                        }
+                      });
+                    },
+                    child: urls == null || i < urls.length - 1
+                        ? const Icon(Icons.forward)
+                        : Text(l10n.introStart),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
-  }
-
-  PageDecoration getPageDecoration() {
-    return const PageDecoration(
-        imagePadding: EdgeInsets.zero,
-        contentMargin: EdgeInsets.zero,
-        pageMargin: EdgeInsets.zero,
-        titlePadding: EdgeInsets.zero,
-        bodyPadding: EdgeInsets.zero,
-        pageColor: Colors.white,
-        bodyFlex: 0,
-        footerFlex: 0,
-        titleTextStyle: TextStyle(
-          fontSize: 0,
-        ),
-        bodyTextStyle: TextStyle(
-          fontSize: 0,
-        ));
   }
 }
