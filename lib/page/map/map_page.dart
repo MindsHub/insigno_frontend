fcb4d6e97e34f001b54ca8df81ca2f1cd8fc60e3
Stypox <stypox@pm.me>
2023-10-15 20:58:21 +0200

    Update OSM url according to issue
    https://github.com/openstreetmap/operations/issues/737

--- lib/page/map/map_page.dart
+++ lib/page/map/map_page.dart
@@ -168,8 +168,7 @@ class _MapPageState extends State<MapPage> with GetItStateMixin<MapPage>, Widget
             }),
         children: [
           TileLayer(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: const ['a', 'b', 'c'],
+            urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
           ),
           MarkerLayer(
               markers: [position?.toLatLng()]
